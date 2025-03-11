cf7bf00c33ef9143168a2bf91f959ed7e04be671
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-03-11 19:47:41 +0530

    operators

---
+++ 02/05.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int num = 54;
+
+    if (num >= 0)
+    {
+        if (num % 2 == 0)
+        {
+            printf("number is even \n");
+        }
+        else
+        {
+            printf("number is odd \n")
+        }
+    }
+    else
+    {
+        printf("number is negative \n");
+    }
+
+    return 0;
+}
