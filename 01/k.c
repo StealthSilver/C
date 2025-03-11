60fa2c0ce420e1710cb6606d8bbb539e0792a858
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-03-11 16:26:54 +0530

    operators

---
+++ 01/k.c
@@ -0,0 +1,15 @@
+// checking the operations with boolean variables
+
+#include <stdio.h>
+
+int main()
+{
+
+    int isSunday = 1;
+    int isRaining = 0;
+
+    printf("%d \n", isSunday && isRaining);
+    printf("%d \n", isSunday || isRaining);
+
+    return 0;
+}
