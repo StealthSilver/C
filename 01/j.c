ec2e6833675a46fab0e70daa935e354c003bf5e7
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-03-11 16:23:17 +0530

    operators

--- 01/j.c
+++ 01/j.c
@@ -1,1 +1,14 @@
 // checking if a no. is divisible by 2
+
+#include <stdio.h>
+
+int main()
+{
+
+    int x;
+    printf("enter a number : ");
+    scanf("%d", &x);
+    printf("%d", x % 2 == 0);
+
+    return 0;
+}
