66f985f55c885bfa2398661e09429ebd9f555e3a
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-03-11 16:03:08 +0530

    operators

--- 01/d.c
+++ 01/d.c
@@ -4,7 +4,9 @@ int main()
 {
 
     printf("%f \n", 4 / 2.0);
-    printf("%f \n", 4 / 2);
+    printf("%d \n", 4 / 2);
+    printf("%d \n", 2 / 3);
+    printf("%f \n", 2.0 / 3);
 
     return 0;
 }
