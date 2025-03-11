eff6828bbec04edc3e9b9b1a81f12f2b94c3c62d
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-03-11 09:55:40 +0530

    init

---
+++ 00/g.c
@@ -0,0 +1,20 @@
+// sum of two values
+
+#include <stdio.h>
+
+int main()
+{
+
+    int a, b;
+
+    printf("enter a : ");
+    scanf("%d", &a);
+
+    printf("enter b : ");
+    scanf("%d", &b);
+
+    int sum = a + b;
+
+    printf("the sum is %d \n", sum);
+    return 0;
+}
