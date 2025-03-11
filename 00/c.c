722f5611b302e09fd005ff7c24030ab3699e4946
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-03-11 09:39:44 +0530

    init

---
+++ 00/c.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 43;
+    float pi = 3.14;
+    char hashtag = '#';
+
+    printf("%d\n", age);     // %d is for integers
+    printf("%f\n", pi);      // %f is for floating-point numbers
+    printf("%c\n", hashtag); // %c is for characters
+    return 0;
+}
