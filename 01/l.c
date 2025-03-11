231d944a84e473724b8d9d8e82d9947c49a01d27
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-03-11 16:29:31 +0530

    operators

---
+++ 01/l.c
@@ -0,0 +1,12 @@
+// checking if the numebr is 2 digit or not
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    printf("enter the number : ");
+    scanf("%d", &x);
+    printf("%d \n", x > 9 && x < 100);
+
+    return 0;
+}
