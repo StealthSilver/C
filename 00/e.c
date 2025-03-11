146737a2c43f19eb9fbcf2342ea6882144e6b89e
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-03-11 09:49:36 +0530

    init

---
+++ 00/e.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int age = 90;
+    float pi = 3.14;
+    char at = '@';
+
+    printf("age is %d \n", age);
+    printf("pi is %f \n", pi);
+    printf("char is %c \n", at);
+}
