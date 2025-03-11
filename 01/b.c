95629b9092f046869ea6dc204b1a71f9cc32363a
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-03-11 15:52:29 +0530

    operators

---
+++ 01/b.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int a = 1, b = 2;
+    int sum = a + b;
+    int mul = a * b;
+    int div = a / b;
+    int x, y = a * b;
+
+    printf("%d \n", sum);
+    printf("%d \n", mul);
+    printf("%d \n", div);
+    printf("%d \n", x);
+    printf("%d \n", y);
+    return 0;
+}
