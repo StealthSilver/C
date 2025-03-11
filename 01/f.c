34cbf1e6682879680ad3a344121498bed3f87c1a
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-03-11 16:08:19 +0530

    operators

--- 01/f.c
+++ 01/f.c
@@ -1,9 +1,18 @@
-#include <iostream>
+#include <stdio.h>
 
 int main()
 {
 
     int a = 4 * 3 / 6 * 2;
-    printf("%d", a);
+
+    int b = 5 * 2 - 3 * 2;
+    int c = 5 * 2 / 3 * 2;
+    int d = 5 * (2 / 2) * 3;
+    int e = 5 + 2 / 2 * 3;
+    printf("%d \n", a);
+    printf("%d \n", b);
+    printf("%d \n", c);
+    printf("%d \n", d);
+    printf("%d \n", e);
     return 0;
 }
