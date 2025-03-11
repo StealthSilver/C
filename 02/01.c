d6f19819c0103aee038bf634ff1ee9737beec007
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-03-11 19:18:34 +0530

    operators

--- 02/01.c
+++ 02/01.c
@@ -3,15 +3,15 @@
 int main()
 {
 
-    int a = 9;
+    int a = 19;
 
     if (a >= 10)
     {
-        printf("Number is greater than 10")
+        printf("Number is greater than 10");
     }
     else
     {
-        print("Number is less than 10")
+        printf("Number is less than 10");
     }
 
     return 0;
