dcbc0d69cb493571ba67c6d0d3ba1af92f57136b
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-03-11 19:15:26 +0530

    operators

---
+++ 02/01.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int a = 9;
+
+    if (a >= 10)
+    {
+        printf("Number is greater than 10")
+    }
+    else
+    {
+        print("Number is less than 10")
+    }
+
+    return 0;
+}
