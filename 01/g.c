7427f20ce763bc495f2baaf3761175ee1d371bab
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-03-11 16:14:17 +0530

    operators

--- 01/g.c
+++ 01/g.c
@@ -1,1 +1,14 @@
 #include <stdio.h>
+
+int main()
+{
+
+    printf("%d \n", 3 == 3);
+    printf("%d \n", 3 == 4);
+    printf("%d \n", 3 > 4);
+    printf("%d \n", 3 >= 3);
+    printf("%d \n", 3 < 3);
+    printf("%d \n", 3 <= 4);
+    printf("%d \n", 3 != 4);
+    return 0;
+}
