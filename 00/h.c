ca6cc5e8db1f69b22121f8afbffe4f127f786b8d
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-03-11 10:02:43 +0530

    init

---
+++ 00/h.c
@@ -0,0 +1,16 @@
+// area of the square
+
+#include <stdio.h>
+
+int main()
+{
+
+    float a;
+    printf("enter the side : ");
+    scanf("%f", &a);
+
+    float area = a * a;
+
+    printf("the area is : %f", area);
+    return 0;
+}
---
+++ 00/i.c
@@ -0,0 +1,15 @@
+// area of the circle
+
+#include <stdio.h>
+
+int main()
+{
+
+    float rad;
+
+    printf("enter radius : ");
+    scanf("%f", &rad);
+
+    printf("the area is : %f", 3.14 * rad * rad);
+    return 0;
+}
