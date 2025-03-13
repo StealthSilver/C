6bbdd9dd9dfc8cd8137ead6684163590e91fd17a
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-03-13 12:34:27 +0530

    functions

--- 04/d.c
+++ 04/d.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 
+int sum(int a, int b)
+{
+    return a + b;
+}
+
 int main()
 {
+
+    int a, b;
+
+    printf("enter the first numeber");
+    scanf("%d", &a);
+
+    printf("enter the second number");
+    scanf("%d", &b);
 
     return 0;
 }
