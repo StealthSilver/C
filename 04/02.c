c007f25d497b1c843c794726a572784fd47506db
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-03-13 11:56:54 +0530

    functions

--- 04/02.c
+++ 04/02.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
+void printHello()
+{
+    printf("hello");
+}
+
+void printBye()
+{
+    printf("Good Bye");
+}
+
 int main()
 {
+    printHello();
+    printBye();
     return 0;
 }
