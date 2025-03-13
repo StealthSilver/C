06038f1f11741458144397678b7bcbecaec40e04
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-03-13 17:22:09 +0530

    functions

--- 04/05.c
+++ 04/05.c
@@ -10,6 +10,6 @@ int main()
 {
     float value = 100.0;
     calculatePrice(value);
-    printf("value is : %f", value);
+    printf("value is : %f", value); // the change in the argument will not reflect in the parameter
     return 0;
 }
