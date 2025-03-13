e698161de0228ac6ca755be4dbaaa8ae4c116bdb
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-03-13 17:21:48 +0530

    functions

--- 04/05.c
+++ 04/05.c
@@ -3,12 +3,13 @@
 void calculatePrice(float value)
 {
     value = value + (0.18 * value);
-    printf("final price is : %f", value);
+    printf("final price is : %f \n", value);
 }
 
 int main()
 {
     float value = 100.0;
     calculatePrice(value);
+    printf("value is : %f", value);
     return 0;
 }
