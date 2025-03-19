6d9676a60a0f8744eea80e097a40126405042e88
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-03-19 20:01:16 +0530

    array

--- 07/07.c
+++ 07/07.c
@@ -2,14 +2,14 @@
 
 int main()
 {
-
     int age = 90;
     int _age = 32;
 
     int *ptr = &age;
     int *_ptr = &_age;
 
-    printf("difference = % u\n", ptr = ptr - _ptr);
+    printf("Difference = %td\n", ptr - _ptr);
+    printf("Comparison = %d\n", ptr == _ptr);
 
     return 0;
 }
