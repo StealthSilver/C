e1fd15aa864a9dc35403a14e2dc74a75e7a9d33b
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-03-19 11:17:21 +0530

    pointer

--- 06/07.c
+++ 06/07.c
@@ -11,12 +11,22 @@ void swap(int a, int b)
     printf("a = %d & b = %d\n", a, b);
 }
 
+void _swap(int *a, int *b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
 int main()
 {
 
     int x = 3, y = 5;
 
     swap(x, y);
+    printf("a = %d & b = %d\n", x, y);
+
+    _swap(x, y);
     printf("a = %d & b = %d\n", x, y);
 
     return 0;
