0ec38e8ffbfecaaf9d0196ac82d296482281db8f
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-03-19 11:59:32 +0530

    pointer

---
+++ 06/10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void findMax(int *a, int *b, int *max)
+{
+    *max = (*a > *b) ? *a : *b;
+}
+
+int main()
+{
+    int num1, num2, max;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    findMax(&num1, &num2, &max);
+
+    printf("The maximum number is: %d\n", max);
+
+    return 0;
+}
