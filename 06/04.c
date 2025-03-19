3cfc21c106d7755e4f875714e24b79197ffbb7a0
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-03-19 11:06:15 +0530

    pointer

--- 06/04.c
+++ 06/04.c
@@ -6,7 +6,10 @@ int main()
     float *ptr = &price;
     float **pptr = &ptr;
 
-    printf("%d\n", **pptr);
+    printf("Value using **pptr: %f\n", **pptr);              //  Dereferencing twice gives the value of price
+    printf("Value using *pptr: %p\n", (void *)*pptr);        //*pptr gives address stored in ptr
+    printf("Address of price using ptr: %p\n", (void *)ptr); // ptr stores address of price
+    printf("Address of ptr using pptr: %p\n", (void *)pptr); // pptr stores address of ptr
 
     return 0;
 }
