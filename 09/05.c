ebf749630e3d735436abddfc81c7eefdb51cec3c
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-03-20 11:23:44 +0530

    structures

---
+++ 09/05.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+struct student
+{
+    int roll;
+    int cgpa;
+    char name[100];
+};
+
+int main()
+{
+    struct student s1 = {1223, 9.5, "pogo"};
+    printf("student roll = %d\n", s1.roll);
+
+    struct student *ptr = &s1;
+    printf("student roll = %d\n", (*ptr).roll);
+    return 0;
+}
