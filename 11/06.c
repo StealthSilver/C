32e2ac37e020e7dea596f74332c1803f4f0f728d
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-03-20 13:38:56 +0530

    DMA

---
+++ 11/06.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int *ptr;
+    ptr = (int *)calloc(5, sizeof(int));
+
+    printf("enter numebrs(5) : ");
+
+    for (int i = 0; i < 5; i++)
+    {
+        scanf("%d", &ptr[i]);
+    }
+
+    ptr = realloc(ptr, 8);
+    printf("enter number(8) : ");
+    for (int i = 0; i < 5; i++)
+    {
+        scanf("d", &ptr[i]);
+    }
+
+    // print
+
+    for (int i = 0; i < 8; i++)
+    {
+        printf("number %d is %d", &ptr[i]);
+    }
+
+    return 0;
+}
